a24c5a46a24c7552c252b5c7fad29533b524aa1d
B-Lokeshwaran <98377427+B-Lokeshwaran@users.noreply.github.com>
2022-03-16 11:29:34 +0530

    Update StudentViewAttendance.jsx

--- src/Components/student/StudentViewAttendance.jsx
+++ src/Components/student/StudentViewAttendance.jsx
@@ -91,7 +91,7 @@ export default function CustomPaginationActionsTable() {
   const [state,setState] = React.useState([])
   React.useEffect(()=>{
     const loadData = async ()=>{
-      const response  = await axios.get('https://attendance-portal-backend.herokuapp.com/myattendance')
+      const response  = await axios.get('https://protected-atoll-20475.herokuapp.com/myattendance')
       setState(response.data)
     }
     loadData();
